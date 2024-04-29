6a14e1b876014a01439119303abd8a82db278591
Oshan <it20070458@my.sliit.lk>
2024-04-29 21:40:54 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+//Assign into localhost ports
+const PORT = process.env.PORT || 8000;
+
+//Json Methods
+app.use(cors());
+app.use(bodyParser.json());
+
+//Database Connection
+const URL = process.env.MONGODB_URL;
+
+//routes
+
+
+//MongoDB configurations
+mongoose.connect(URL, {
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
+
+//Mongoose Connection
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB Connection Successful!");
+});
+
+//Run on port
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port number : ${PORT}`);
+});
